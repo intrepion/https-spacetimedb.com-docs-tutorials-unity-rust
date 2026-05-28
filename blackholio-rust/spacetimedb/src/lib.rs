20783f05383d99fe736898e6d2071628074ec967
Oliver Forral <oliver.forral@myreason.ai>
2026-05-28 11:30:05 -0700

    Spawning Food

--- blackholio-rust/spacetimedb/src/lib.rs
+++ blackholio-rust/spacetimedb/src/lib.rs
@@ -1,6 +1,14 @@
 use std::time::Duration;
 use spacetimedb::{rand::Rng, Identity, SpacetimeType, ReducerContext, ScheduleAt, Table, Timestamp};
 
+#[spacetimedb::table(accessor = spawn_food_timer, scheduled(spawn_food))]
+pub struct SpawnFoodTimer {
+    #[primary_key]
+    #[auto_inc]
+    scheduled_id: u64,
+    scheduled_at: spacetimedb::ScheduleAt,
+}
+
 // We're using this table as a singleton, so in this table
 // there only be one element where the `id` is 0.
 #[spacetimedb::table(accessor = config, public)]
@@ -57,8 +65,70 @@ pub struct Player {
     name: String,
 }
 
+// Note the `init` parameter passed to the reducer macro.
+// That indicates to SpacetimeDB that it should be called
+// once upon database creation.
+#[spacetimedb::reducer(init)]
+pub fn init(ctx: &ReducerContext) -> Result<(), String> {
+    log::info!("Initializing...");
+    ctx.db.config().try_insert(Config {
+        id: 0,
+        world_size: 1000,
+    })?;
+    ctx.db.spawn_food_timer().try_insert(SpawnFoodTimer {
+        scheduled_id: 0,
+        scheduled_at: ScheduleAt::Interval(Duration::from_millis(500).into()),
+    })?;
+    Ok(())
+}
+
 #[spacetimedb::reducer(client_connected)]
 pub fn connect(ctx: &ReducerContext) -> Result<(), String> {
     log::debug!("{} just connected.", ctx.sender());
+    Ok(())
+}
+
+const FOOD_MASS_MIN: i32 = 2;
+const FOOD_MASS_MAX: i32 = 4;
+const TARGET_FOOD_COUNT: usize = 600;
+
+fn mass_to_radius(mass: i32) -> f32 {
+    (mass as f32).sqrt()
+}
+
+#[spacetimedb::reducer]
+pub fn spawn_food(ctx: &ReducerContext, _timer: SpawnFoodTimer) -> Result<(), String> {
+    if ctx.db.player().count() == 0 {
+        // Are there no logged in players? Skip food spawn.
+        return Ok(());
+    }
+
+    let world_size = ctx
+        .db
+        .config()
+        .id()
+        .find(0)
+        .ok_or("Config not found")?
+        .world_size;
+
+    let mut rng = ctx.rng();
+    let mut food_count = ctx.db.food().count();
+    while food_count < TARGET_FOOD_COUNT as u64 {
+        let food_mass = rng.gen_range(FOOD_MASS_MIN..FOOD_MASS_MAX);
+        let food_radius = mass_to_radius(food_mass);
+        let x = rng.gen_range(food_radius..world_size as f32 - food_radius);
+        let y = rng.gen_range(food_radius..world_size as f32 - food_radius);
+        let entity = ctx.db.entity().try_insert(Entity {
+            entity_id: 0,
+            position: DbVector2 { x, y },
+            mass: food_mass,
+        })?;
+        ctx.db.food().try_insert(Food {
+            entity_id: entity.entity_id,
+        })?;
+        food_count += 1;
+        log::info!("Spawned food! {}", entity.entity_id);
+    }
+
     Ok(())
 }
