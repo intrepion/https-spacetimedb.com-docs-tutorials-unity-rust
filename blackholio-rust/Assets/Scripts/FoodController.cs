3952d641d6c9d2fe32955378a675afdbbc9d39e4
Oliver Forral <oliver.forral@myreason.ai>
2026-05-28 18:29:21 -0700

    FoodController

--- blackholio-rust/Assets/Scripts/FoodController.cs
+++ blackholio-rust/Assets/Scripts/FoodController.cs
@@ -1,16 +1,23 @@
+using SpacetimeDB.Types;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class FoodController : MonoBehaviour
+public class FoodController : EntityController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static Color[] ColorPalette = new[]
     {
-        
-    }
+        (Color)new Color32(119, 252, 173, 255),
+        (Color)new Color32(76, 250, 146, 255),
+        (Color)new Color32(35, 246, 120, 255),
 
-    // Update is called once per frame
-    void Update()
+        (Color)new Color32(119, 251, 201, 255),
+        (Color)new Color32(76, 249, 184, 255),
+        (Color)new Color32(35, 245, 165, 255),
+    };
+
+    public void Spawn(Food food)
     {
-        
+        base.Spawn(food.EntityId);
+        SetColor(ColorPalette[EntityId % ColorPalette.Length]);
     }
 }
