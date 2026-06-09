800df558c2267357e9d0b2e6e561d230c6221248
Oliver Forral <intrepion@gmail.com>
2026-06-08 21:39:09 -0600

    Creating Entities

--- blackholio-rust/blackholio-server/spacetimedb/src/lib.rs
+++ blackholio-rust/blackholio-server/spacetimedb/src/lib.rs
@@ -9,3 +9,39 @@ pub struct Config {
     pub id: i32,
     pub world_size: i64,
 }
+
+// This allows us to store 2D points in tables.
+#[derive(SpacetimeType, Clone, Debug)]
+pub struct DbVector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[spacetimedb::table(accessor = entity, public)]
+#[derive(Debug, Clone)]
+pub struct Entity {
+    // The `auto_inc` attribute indicates to SpacetimeDB that
+    // this value should be determined by SpacetimeDB on insert.
+    #[auto_inc]
+    #[primary_key]
+    pub entity_id: i32,
+    pub position: DbVector2,
+    pub mass: i32,
+}
+
+#[spacetimedb::table(accessor = circle, public)]
+pub struct Circle {
+    #[primary_key]
+    pub entity_id: i32,
+    #[index(btree)]
+    pub player_id: i32,
+    pub direction: DbVector2,
+    pub speed: f32,
+    pub last_split_time: Timestamp,
+}
+
+#[spacetimedb::table(accessor = food, public)]
+pub struct Food {
+    #[primary_key]
+    pub entity_id: i32,
+}
